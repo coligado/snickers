6d17cf5f744e22486dd15978df1f2bb0da04285d
Lea Coligado <coligado@coligado-mbp.thefacebook.com>
2013-07-22 10:48:43 -0700

    first commit of snickers

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SnickersMaze
+//
+//  Created by Lea Coligado on 7/22/13.
+//  Copyright (c) 2013 Lea Coligado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SnickersMazeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SnickersMazeAppDelegate class]));
+    }
+}
