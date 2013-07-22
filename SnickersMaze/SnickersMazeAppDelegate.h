385aa2faa3b4ad947b5ea8497a3fbd71345d72fc
Lea Coligado <coligado@coligado-mbp.thefacebook.com>
2013-07-22 10:10:08 -0700

    Initial Commit

---
+++ SnickersMaze/SnickersMazeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SnickersMazeAppDelegate.h
+//  SnickersMaze
+//
+//  Created by Lea Coligado on 7/22/13.
+//  Copyright (c) 2013 Lea Coligado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SnickersMazeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SnickersMazeTests/SnickersMazeTests.m
@@ -0,0 +1,36 @@
+//
+//  SnickersMazeTests.m
+//  SnickersMazeTests
+//
+//  Created by Lea Coligado on 7/22/13.
+//  Copyright (c) 2013 Lea Coligado. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SnickersMazeTests : XCTestCase
+
+@end
+
+@implementation SnickersMazeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
